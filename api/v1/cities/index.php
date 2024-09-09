9f168c81ff1ce91303d33bd88185186b61bbdb76
haakel <haakel213@gmail.com>
2024-09-09 19:08:24 +0330

    s10 - e07 - part1

--- api/v1/cities/index.php
+++ api/v1/cities/index.php
@@ -1,3 +1,5 @@
 <?php 
+include_once "../../../appiran/services/CityService.php";
 echo "city endpoint is here......";
- 
+$c= new CityService();
+$c->getCities([]);
---
+++ appiran/services/CityService.php
@@ -0,0 +1,9 @@
+<?php
+
+
+class CityService{
+
+    public function getCities($data){
+        echo "{c1,c2,c3}";
+    }
+}
